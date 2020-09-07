25001d83d346a8161fb6de1af245bedf63e00c15
Opilane TTHK <opilane@tthk.ee>
2020-09-07 14:28:23 +0300

    Commit My Hello World App

---
+++ Week2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What`s your name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+            Console.WriteLine("What is your age");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int YearOfBirth;
+            YearOfBirth = Int32.Parse(UserInput);
+            int Age = 2020 - YearOfBirth;
+            Console.WriteLine("You were born in " + Age);
+
+
+
+
+        }
+
+    }
+}
